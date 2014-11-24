04b7281dc0460012d2830529a510085567e8c945
Jose de la paz <the_joetkd@hotmail.es>
2014-11-23 21:48:44 -0600

    Consulta de usuarios completo y funcionando :)

--- GymJoe/menu.php
+++ GymJoe/menu.php
@@ -9,7 +9,7 @@
                     <li id="elementos_menu"><a  href="instalaciones.php">Instalaciones |</a></li>
                     <li id="elementos_menu"><a  href="blog.php">Blog |</a></li>
                     <li id="elementos_menu"><a href="promociones.php">Promociones |</a></li>
-                    <li id="elementos_menu"><a href="login.php">Iniciar Sesion</a></li>
+                    <li id="elementos_menu"><a href="login.php">Iniciar Sesion |</a></li>
                     <li id="elementos_menu"><a href="conUsuario.php">Consultar clietes</a></li>
                     </ul>
                 </div>
