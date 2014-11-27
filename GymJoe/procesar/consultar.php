d8beabcdb859fffea6d892b4b259f3ab18713649
Mario Arturo Zavaleta Bermúdez <tutuyo_92@hotmail.com>
2014-11-27 12:11:37 -0600

    Se modifico productos y se agrego productosall donde se muestran todos los productos y posteriormente se agregaran al carro de compras

--- GymJoe/procesar/consultar.php
+++ GymJoe/procesar/consultar.php
@@ -29,6 +29,146 @@ mysqli_close($conexion);
 mysqli_free_result($consulta);
 }
 
+public function consultarProductos(){
+	$conexion = mysqli_connect(config::$servidor, config::$usuario,config::$password,config::$baseDeDatos);
+	$consulta = mysqli_query($conexion, "select * from productos");
+	while ($fila = mysqli_fetch_array($consulta)) {
+		echo "<tr>
+				<td>$fila[0]</td>
+				<td>$fila[1]</td>
+				<td>$fila[2]</td>
+				<td>$fila[3]</td>
+				<td>$fila[4]</td>
+				<td>$fila[5]</td>
+				<td>$fila[6]</td>
+				<td><img src='fotos/$fila[7]' width'100px' heigth'100px'></td>
+				<td>$fila[8]</td>
+			  </tr>";
+	}
+	mysqli_close($conexion);
+	mysqli_free_result($consulta);
+}
+
+public function consultarquema(){
+	$conexion = mysqli_connect(config::$servidor, config::$usuario,config::$password,config::$baseDeDatos);
+	$consulta = mysqli_query($conexion, "select * from productos where tipo='Quema Grasa'");
+	while ($fila = mysqli_fetch_array($consulta)) {				
+		echo "
+		<figure id='prod'>
+		<img src='fotos/$fila[7]' width='60%' heigth='60%'/>
+			  <figcaption>
+			  <p>$fila[1]</p>
+			  <p>$fila[5]</p>
+			  <p>$fila[3]</p>			  
+			  <button class='botoncito'>Agregar</button>
+			  </figcaption>
+			  </figure>
+			  ";				  
+	}
+	mysqli_close($conexion);
+	mysqli_free_result($consulta);
+}
+
+public function consultaraumen(){
+	$conexion = mysqli_connect(config::$servidor, config::$usuario,config::$password,config::$baseDeDatos);
+	$consulta = mysqli_query($conexion, "select * from productos where tipo='Aumenta Masa'");
+	while ($fila = mysqli_fetch_array($consulta)) {				
+		echo "
+		<figure id='prod'>
+		<img src='fotos/$fila[7]' width='60%' heigth='60%'/>
+			  <figcaption>
+			  <p>$fila[1]</p>
+			  <p>$fila[5]</p>
+			  <p>$fila[3]</p>			  
+			  <button class='botoncito'>Agregar</button>
+			  </figcaption>
+			  </figure>
+			  ";				  
+	}
+	mysqli_close($conexion);
+	mysqli_free_result($consulta);
+}
+
+public function consultarnos(){
+	$conexion = mysqli_connect(config::$servidor, config::$usuario,config::$password,config::$baseDeDatos);
+	$consulta = mysqli_query($conexion, "select * from productos where tipo='NOS'");
+	while ($fila = mysqli_fetch_array($consulta)) {				
+		echo "
+		<figure id='prod'>
+		<img src='fotos/$fila[7]' width='60%' heigth='60%'/>
+			  <figcaption>
+			  <p>$fila[1]</p>
+			  <p>$fila[5]</p>
+			  <p>$fila[3]</p>			  
+			  <button class='botoncito'>Agregar</button>
+			  </figcaption>
+			  </figure>
+			  ";				  
+	}
+	mysqli_close($conexion);
+	mysqli_free_result($consulta);
+}
+
+public function consultarhc(){
+	$conexion = mysqli_connect(config::$servidor, config::$usuario,config::$password,config::$baseDeDatos);
+	$consulta = mysqli_query($conexion, "select * from productos where tipo='HC'");
+	while ($fila = mysqli_fetch_array($consulta)) {				
+		echo "
+		<figure id='prod'>
+		<img src='fotos/$fila[7]' width='60%' heigth='60%'/>
+			  <figcaption>
+			  <p>$fila[1]</p>
+			  <p>$fila[5]</p>
+			  <p>$fila[3]</p>			  
+			  <button class='botoncito'>Agregar</button>
+			  </figcaption>
+			  </figure>
+			  ";				  
+	}
+	mysqli_close($conexion);
+	mysqli_free_result($consulta);
+}
+
+public function consultarcrea(){
+	$conexion = mysqli_connect(config::$servidor, config::$usuario,config::$password,config::$baseDeDatos);
+	$consulta = mysqli_query($conexion, "select * from productos where tipo='Creatine'");
+	while ($fila = mysqli_fetch_array($consulta)) {				
+		echo "
+		<figure id='prod'>
+		<img src='fotos/$fila[7]' width='60%' heigth='60%'/>
+			  <figcaption>
+			  <p>$fila[1]</p>
+			  <p>$fila[5]</p>
+			  <p>$fila[3]</p>			  
+			  <button class='botoncito'>Agregar</button>
+			  </figcaption>
+			  </figure>
+			  ";				  
+	}
+	mysqli_close($conexion);
+	mysqli_free_result($consulta);
+}
+
+public function consultarbus(){
+	$conexion = mysqli_connect(config::$servidor, config::$usuario,config::$password,config::$baseDeDatos);
+	$consulta = mysqli_query($conexion, "select * from productos where tipo='HC'");
+	while ($fila = mysqli_fetch_array($consulta)) {				
+		echo "
+		<figure id='prod'>
+		<img src='fotos/$fila[7]' width='60%' heigth='60%'/>
+			  <figcaption>
+			  <p>$fila[1]</p>
+			  <p>$fila[5]</p>
+			  <p>$fila[3]</p>			  
+			  <button class='botoncito'>Agregar</button>
+			  </figcaption>
+			  </figure>
+			  ";				  
+	}
+	mysqli_close($conexion);
+	mysqli_free_result($consulta);
+}
+
 public function consultarLogin($nick,$pass){
 $res = false;
 $conexion =  mysqli_connect(config::$servidor, config::$usuario, config::$password, config::$baseDeDatos);
