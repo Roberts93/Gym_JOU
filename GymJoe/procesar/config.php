2f7ec3edb99fddbe4129d5a68ff5b9631c6dd7cf
Mario Arturo Zavaleta Bermúdez <tutuyo_92@hotmail.com>
2014-12-05 00:45:22 -0600

    modificar productos

--- GymJoe/procesar/config.php
+++ GymJoe/procesar/config.php
@@ -2,7 +2,12 @@
 class config{ 
 static $servidor = "localhost";
 static $usuario = "root";
+<<<<<<< HEAD
+static $password = "";
+static $baseDeDatos = "gym";
+=======
 static $password = "1234";
 static $baseDeDatos = "GymJoe";
+>>>>>>> ca4850cd6c54d75fc1eb133e3f740688d139f9b2
 }
  ?>
