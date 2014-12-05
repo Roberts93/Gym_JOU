eadaae3cce25a18e6578c713a52c0c4dd5d562ab
Ignacio10 <ignacio_0731@hotmail.com>
2014-12-05 07:00:04 -0600

    implementación   un carrusel de  imagenes del apartado instalaciones...

--- GymJoe/instalaciones.php
+++ GymJoe/instalaciones.php
@@ -2,10 +2,16 @@
     <title>Gym Joe</title>
     <link rel="stylesheet" type="text/css" href="css/estructura.css">
     <link rel="stylesheet" type="text/css" href="textos.css">
-    <link rel="stylesheet" type="text/css" href="css/menu2.css">
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
     <link rel="stylesheet" type="text/css" href="css/pie_pagina.css">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,800' rel='stylesheet' type='text/css' />
-    <link rel="stylesheet" type="text/css" href="css/Instalaciones.css">
+    <link rel="stylesheet" type="text/css" href="css/instalaciones.css">
+    <link rel="stylesheet" href="slick-master/slick/slick.css">
+	<link rel="stylesheet" href="css/estiloCarrusel_Rutinas.css">
+
+
+
+
 </head>
 <body>
 
@@ -18,13 +24,46 @@
     <hr id="sep" width="100%" height="100" color="blue">
 
 
-    <div id="caja_arriba">
-    <h2><center style="color: black;">Conoce nuestras instalaciones </center></h2>
-    <header>
-<img  style="float" src="img/Instala.jpg" id="tama" alt="" controls>
-<p></p>
+    
+
+
+
+
+
+
 
 </div>
+
+<div id="fondo">
+<div class="multiple-items">
+	<div><img src="img/Carrusel_Instalaciones/Instalaciones.jpg" class="tam_imagen"></div>
+	<div><img src="img/Carrusel_Instalaciones/2.jpg" class="tam_imagen"></div>
+	<div><img src="img/Carrusel_Instalaciones/3.jpg" class="tam_imagen" ></div>
+	<div><img src="img/Carrusel_Instalaciones/4.jpg" class="tam_imagen"></div>
+	<div><img src="img/Carrusel_Instalaciones/5.jpg" class="tam_imagen"></div>
+	<div><img src="img/Carrusel_Instalaciones/6.jpg" class="tam_imagen"></div>
+	<div><img src="img/Carrusel_Instalaciones/7.jpg" class="tam_imagen"></div>
+	<div><img src="img/Carrusel_Instalaciones/8.jpg" class="tam_imagen" ></div>
+	<div><img src="img/Carrusel_Instalaciones/9.jpg" class="tam_imagen"></div>
+	<div><img src="img/Carrusel_Instalaciones/10.jpg" class="tam_imagen"></div>
+	
+</div>
+<script rel="text/javascript" src="jquery-2.1.1.min.js"></script>
+     <script rel="text/javascript" src="slick-master/slick/slick.js"></script>
+	<script type="text/javascript">
+  $('.multiple-items').slick({
+  infinite: true,
+ // slidesToShow: 4,
+ // slidesToScroll: 4
+});
+
+</script>
+</div>
+
+
+
+
+
 </header>
 <hr>
 <br>
@@ -91,6 +130,13 @@
 <img src="img/bailee.jpg" width="300" height="180" controls>
 <figcaption><b></b></figcaption>
 </div>
+
+
+
+
+
+
+
 
 
 </div>
