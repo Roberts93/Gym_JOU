6fb942e955188ce5e4f80424271447c183cdbf60
Jose de la paz <the_joetkd@hotmail.es>
2014-12-09 01:35:44 -0600

    blog actual

--- GymJoe/ConectaBlog.php
+++ GymJoe/ConectaBlog.php
@@ -18,17 +18,19 @@ include('menu.php');
 	<img  style="float"  src="img/CarrucelPrincipal/Blog.jpg" id="tamaImagen" alt="" controls style="width: 99.7px" style="height: 53px">		
 	</div>
 
-
+<div id="cuadroEntradas">
+<CENTER><h1 style="color:black">ENTRADAS RECIENTES</h1></CENTER>
 <?php require("../../wordpress/wp-blog-header.php"); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
  
-<?php the_date('','<h3 style="color:black">','</h3>'); ?>
+<?php the_date('','<h3 id="fechas">','</h3>'); ?>
  
 <div class="post" id="post-<?php the_ID(); ?>">
 	 <h3 class="storytitle"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
  
-	<div class="storycontent" style="color:black">
+	<div class="storycontent" id="informacion">
 		<?php the_excerpt(__('(more...)')); echo " . . ."; ?>
+		<hr>
 	</div>
  
 </div>
@@ -36,7 +38,7 @@ include('menu.php');
 <?php endwhile; else: ?>
 <p><?php _e('Lo siento. Ahora mismo no hay ningún post publicado'); ?></p>
 <?php endif; ?>
-
+</div>
 
 </div>
 
