60b3c8d4196c9847f4e2fdc4f482a7bf74640d1b
Oscar Daniel Rangel Martinez <gotiel.orm@gmail.com>
2014-12-09 22:11:59 -0600

    cerrado de session!

--- GymJoe/login.php
+++ GymJoe/login.php
@@ -16,7 +16,7 @@ include('menu.php');
 ?>	
 
 
-<hr width="100%" height="100" color="red">
+<hr width="100%" height="100" color="#ccc">
 <hr id="hr_separador" width="100%" height="100" color="blue">
 <div class="row">
 <div class="large-6 large-centered columns">
@@ -51,9 +51,7 @@ include('menu.php');
 		echo '<div data-alert class="alert-box success round">El usuario no existe, verifique su contraseña y su usuario</div>';
 	}
 }
-	?>
-			
-
+	?>	
 	</form>
 	</div>
 </div>	
